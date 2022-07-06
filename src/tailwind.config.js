b29bae896a9e927fff2b6748d61dfd52eda7cfe1
ShenyiCui <shenyicui@outlook.com>
2022-07-07 00:46:18 +0800

    add fonts and add tailwind color

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  important: true,
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: { Inter: ['Inter', 'sans-serif'] },
     extend: {},
+    colors: {
+      grey: '#f5f5f5',
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
